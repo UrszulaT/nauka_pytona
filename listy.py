9c5bcbc58253429313becd719fa0f8069892b6ef
UrszulaT <UrszulaT@users.noreply.github.com>
2018-12-09 14:36:25 +0100

    list

---
+++ listy.py
@@ -0,0 +1,11 @@
+samolot = {'name' : 'boeing',
+        'przebieg' : '10000',
+        'type' : 'pasazerski'}
+
+for key, value in samolot.iteritems():
+    print("{0}:{1}".format(key, value))
+
+for key in samolot:
+    print("{0}:{1}".format(key, samolot[key]))
+
+print(samolot['nieznany_klucz'])
