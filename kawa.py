e68f4bc2ba596d14f2608613a722155df5bd4cf4
UrszulaT <UrszulaT@users.noreply.github.com>
2018-12-09 11:47:37 +0100

    kawa

---
+++ kawa.py
@@ -0,0 +1,10 @@
+kawa = [ 'espresso' , 'cappuccino' , 'caffe latte' , 'flat white' , 'americano']
+
+print(kawa[2])
+print(kawa[4])
+
+for s in kawa:
+    print(s)
+
+for idx in range( len(kawa)):
+    print("idx: " + " : " + kawa[idx])
